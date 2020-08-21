afe3fe7a7876b2d1e8061f0209d663f4a93f36a1
cadenzah <cadenzah93@gmail.com>
2020-08-21 13:31:03 +0900

    separated logic for initializing player instance into util module

---
+++ lib/utils/initializePlayer.ts
@@ -0,0 +1,28 @@
+import videojs, { VideoJsPlayer } from 'video.js';
+import Player from '../index';
+
+function initializePlayer(playerRef: HTMLVideoElement | null, playerOptions: videojs.PlayerOptions, manualPlugins: Array<Player.IVideoJsPlugin>): VideoJsPlayer {
+  const player: Player.IVideoJsPlayer = videojs(
+    playerRef,
+    playerOptions
+  );
+
+  // Initialization of src and poster if exists
+  const videoSrc = playerOptions.src;
+  const videoPoster = playerOptions.poster;
+  videoSrc && player.src(videoSrc);
+  videoPoster && player.poster(videoPoster);
+
+  // Registration and Option initialization of manual plugins
+  manualPlugins.map(element => {
+    element.plugin && videojs.registerPlugin(
+      element.name,
+      element.plugin,
+    );
+    player[element.name](player, element.options);
+  });
+
+  return player;
+}
+
+export default initializePlayer;
